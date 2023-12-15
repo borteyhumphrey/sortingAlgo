b4ad6a5e98500f6086053f7fc7560c55a4506ddc
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-12-15 18:26:10 -0500

    Uncomment main function

--- linSearch.py
+++ linSearch.py
@@ -35,46 +35,45 @@ def linear_search(arr, key):
             return i
     return -1
 
-print(inspect.getdoc(linear_search))
 
-# def main ():
-#     #get file path from user
-#     file_path = args.file
+def main ():
+    #get file path from user
+    file_path = args.file
 
-#     #read file into dataframe
-#     df = pd.read_csv(file_path, header=None)
+    #read file into dataframe
+    df = pd.read_csv(file_path, header=None)
 
-#     #convert to list
-#     arr = df.values.tolist()
-#     #save list items into new list
-#     new_list = []
-#     for item in arr:
-#         new_list.append(item)
+    #convert to list
+    arr = df.values.tolist()
+    #save list items into new list
+    new_list = []
+    for item in arr:
+        new_list.append(item)
     
-#     #initialize runnning time
-#     total_run_time = 0
+    #initialize runnning time
+    total_run_time = 0
 
-#     #random ISBN selection
-#     for arr in range(len(new_list)):
-#         choose_book_record = random.choice(new_list)
-#         select_ISBN = choose_book_record[0]
+    #random ISBN selection
+    for arr in range(len(new_list)):
+        choose_book_record = random.choice(new_list)
+        select_ISBN = choose_book_record[0]
 
-#         start_time = time.time() #get start time
-#         search = linear_search(new_list, select_ISBN) #search randomly selected ISBN
-#         end_time = time.time() #get end time
+        start_time = time.time() #get start time
+        search = linear_search(new_list, select_ISBN) #search randomly selected ISBN
+        end_time = time.time() #get end time
 
-#         #compute running time
-#         running_time = (end_time - start_time) * 1000
-#         total_run_time += running_time
+        #compute running time
+        running_time = (end_time - start_time) * 1000
+        total_run_time += running_time
 
-#         #print ISBN and position in list
-#         print(f"{select_ISBN} found at index {search}")
+        #print ISBN and position in list
+        print(f"{select_ISBN} found at index {search}")
     
-#     print()
+    print()
 
-#     #print size of list and runtime
-#     print(f"Data size: {len(new_list)}, Run time: {total_run_time:.0f}ms")
+    #print size of list and runtime
+    print(f"Data size: {len(new_list)}, Run time: {total_run_time:.0f}ms")
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
