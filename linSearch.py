f4b946922fabf2b68fd7c8dc2731fa755952e324
Humphrey Borketey <borteyhumphrey@gmail.com>
2023-12-15 17:50:17 -0500

    Add doc string to linear_search function

--- linSearch.py
+++ linSearch.py
@@ -2,6 +2,7 @@ import pandas as pd
 import time
 import random
 import argparse
+import inspect
 
 parser = argparse.ArgumentParser(description='Perform linear search on a list of fictitious book records')
 
@@ -15,51 +16,65 @@ Author: Humphrey B. Borketey, borteyhumphrey@gmail.com
 '''
 
 #linear seach algorithm
-def linearSearch(arr, key):
+def linear_search(arr, key):
+    """Take a list of fictitious books `arr` and search for an ISBN `key` using Linear Search.
+
+    Args:
+        arr (list): The list of books.
+        key (int): The ISBN to search for.
+
+    Returns:
+        list
+    
+    ValueError:
+        Raises
+            If ISBN `key` not found.
+    """
     for i in range(len(arr)):
         if arr[i][0] == key:
             return i
     return -1
-        
 
-def main ():
-    #get file path from user
-    file_path = args.file
+print(inspect.getdoc(linear_search))
 
-    #read file into dataframe
-    df = pd.read_csv(file_path, header=None)
+# def main ():
+#     #get file path from user
+#     file_path = args.file
 
-    #convert to list
-    arr = df.values.tolist()
-    #save list items into new list
-    new_list = []
-    for item in arr:
-        new_list.append(item)
+#     #read file into dataframe
+#     df = pd.read_csv(file_path, header=None)
+
+#     #convert to list
+#     arr = df.values.tolist()
+#     #save list items into new list
+#     new_list = []
+#     for item in arr:
+#         new_list.append(item)
     
-    #initialize runnning time
-    total_run_time = 0
+#     #initialize runnning time
+#     total_run_time = 0
 
-    #random ISBN selection
-    for arr in range(len(new_list)):
-        choose_book_record = random.choice(new_list)
-        select_ISBN = choose_book_record[0]
+#     #random ISBN selection
+#     for arr in range(len(new_list)):
+#         choose_book_record = random.choice(new_list)
+#         select_ISBN = choose_book_record[0]
 
-        start_time = time.time() #get start time
-        search = linearSearch(new_list, select_ISBN) #search randomly selected ISBN
-        end_time = time.time() #get end time
+#         start_time = time.time() #get start time
+#         search = linear_search(new_list, select_ISBN) #search randomly selected ISBN
+#         end_time = time.time() #get end time
 
-        #compute running time
-        running_time = (end_time - start_time) * 1000
-        total_run_time += running_time
+#         #compute running time
+#         running_time = (end_time - start_time) * 1000
+#         total_run_time += running_time
 
-        #print ISBN and position in list
-        print(f"{select_ISBN} found at index {search}")
+#         #print ISBN and position in list
+#         print(f"{select_ISBN} found at index {search}")
     
-    print()
+#     print()
 
-    #print size of list and runtime
-    print(f"Data size: {len(new_list)}, Run time: {total_run_time:.0f}ms")
+#     #print size of list and runtime
+#     print(f"Data size: {len(new_list)}, Run time: {total_run_time:.0f}ms")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
 
